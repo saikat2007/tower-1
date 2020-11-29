2e886887301f26761743fec99ddac5a1accfa9a5
saikat2007 <67913259+saikat2007@users.noreply.github.com>
2020-11-29 19:44:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,57 @@
-var block,polygon
 
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground,stand1,stand2,slingshot,polygon_img
+function preload(){
+  polygon_img=loadImage("polygon.png")
+}
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+ Engine.run(engine)
+ground=new Ground();
+stand1=new Stand(390,300,250,10)
+stand2=new Stand(700,200,200,10)
+slingShot=new SlingShot(this.ball,{x:100,y:200})
+ 
 
 
-  boxbaseSprite=createSprite(boxPosition.X,boxY,600,-30)
-  boxbaseSprite.shapeColor=color(255,0,0);
-  boxbaseBody=Bodies.rectangle(boxPosition-100,boxY,20,100,{isStatic:true})
-  World.add(world,boxbaseBody)
+  block1 = new Block(330,235,30,40);
+  block2= new Block(360,235,30,40);
+  block3= new Block(390,235,30,40);
+  block4= new Block(420,235,30,40);
+  block5= new Block(450,235,30,40);
+  block6= new Block(360,235,30,40);
+  block7= new Block(390,195,30,40);
+  block8= new Block(420,195,30,40);
+  block9 = new Block(390,195,30,40);
+
+}
+function draw (){
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+
+  block7.display();
+  block8.display();
+  block9.display();
+  slingshot.display();
+  block.display();
+  ground.display();
+  stand.display();
 
 
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11= new Block(420,235,30,40);
-  block12= new Block(450,235,30,40);
-  block13= new Block(360,235,30,40);
-  block14= new Block(390,195,30,40);
-  block15= new Block(420,195,30,40);
-  block16 = new Block(390,195,30,40);
-
-  
+}
   
   function mouseDragged(){
    
@@ -47,7 +77,7 @@ function setup() {
 
 
 
-}
+
 
 function draw() {
   background(255,255,255);  
